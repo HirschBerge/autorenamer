ca05adcd29424929313ca9b8cfa12565351b4a82
HirschBerge <hirschy.kirkwood@proton.me>
2024-10-31 15:49:59 -0400

    script fix

--- benchmarks.sh
+++ benchmarks.sh
@@ -4,6 +4,6 @@ folder_path="test/"
 if [ ! -d "$SCRIPT_DIR/$folder_path" ]; then
     mkdir -p "$SCRIPT_DIR/$folder_path"
 fi
-cd $SCRIPT_DIR/$folder_path
+cd "$SCRIPT_DIR"/$folder_path || exit
 rm ** -f ;touch Episode\ {50000..1}.mp4
 # rm -rf $SCRIPT_DIR/test || true
